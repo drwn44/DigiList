0450ec99e599c630ae872ba18e79b39475d34204
drwn44 <labbalazs@mailbox.unideb.hu>
2026-03-18 12:24:44 +0100

    refactor: necessary refactoring for EAS build

--- app.config.js
+++ app.config.js
@@ -1,7 +1,7 @@
 export default {
     expo: {
         name: "DigiList",
-        slug: "DigiList",
+        slug: "digilist",
         version: "1.0.0",
         orientation: "portrait",
         icon: "./assets/icon.png",
@@ -17,17 +17,23 @@ export default {
             supportsTablet: true
         },
         android: {
+            package: "com.drwn.digilist",
             adaptiveIcon: {
                 foregroundImage: "./assets/adaptive-icon.png",
                 backgroundColor: "#ffffff"
             },
-            edgeToEdgeEnabled: true
+            edgeToEdgeEnabled: true,
         },
         web: {
             favicon: "./assets/favicon.png"
         },
         extra: {
             firebaseApiKey: process.env.FIREBASE_API_KEY,
+            firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
+            firebaseProjectId: process.env.FIREBASE_PROJECT_ID,
+            firebaseStorageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+            firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+            firebaseAppId: process.env.FIREBASE_APP_ID,
             groqApiKey: process.env.GROQ_API_KEY,
         }
     }
--- src/firebase.js
+++ src/firebase.js
@@ -8,12 +8,11 @@ const {extra} = Constants.expoConfig;
 
 const firebaseConfig = {
     apiKey: extra.firebaseApiKey,
-    authDomain: "digilist-19aa4.firebaseapp.com",
-    projectId: "digilist-19aa4",
-    storageBucket: "digilist-19aa4.firebasestorage.app",
-    messagingSenderId: "430919264998",
-    appId: "1:430919264998:web:60abf4c716fcdeab19e927",
-    measurementId: "G-7QJ1W7QZK1"
+    authDomain: extra.firebaseAuthDomain,
+    projectId: extra.firebaseProjectId,
+    storageBucket: extra.firebaseStorageBucket,
+    messagingSenderId: extra.firebaseMessagingSenderId,
+    appId: extra.firebaseAppId,
 };
 
 const app = initializeApp(firebaseConfig);
@@ -21,5 +20,3 @@ export const auth = initializeAuth(app, {
     persistence: getReactNativePersistence(AsyncStorage)
 });
 export const db = getFirestore(app);
-
-console.log('auth init:', auth);
