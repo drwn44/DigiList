b9b896650b05c3d649b3da06e85dbf3dc330b60f
drwn44 <labbalazs@mailbox.unideb.hu>
2026-03-19 22:57:58 +0100

    feat: add persistent local cache

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import { initializeFirestore, persistentLocalCache } from 'firebase/firestore';
 import Constants from 'expo-constants'
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
@@ -19,4 +19,6 @@ const app = initializeApp(firebaseConfig);
 export const auth = initializeAuth(app, {
     persistence: getReactNativePersistence(AsyncStorage)
 });
-export const db = getFirestore(app);
+export const db = initializeFirestore(app, {
+    localCache: persistentLocalCache()
+});
